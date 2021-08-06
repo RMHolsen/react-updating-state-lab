c1d4e10f17c006ec05f89be4d2256ed98a4c38a3
rmholsen <jaguar.kitty@gmail.com>
2021-08-06 18:15:09 -0400

    Completed

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,31 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+// remember that it is in fact 'onClick' and not 'onclick'
+// remember that when you're setting the state based on the previous state
+// it is stateKey: previousState.stateKey + change
+// the stateKey must be on there.
+
+class DigitalClicker extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0
+        };
+    }
+
+    clickHandler = () => {
+        this.setState(previousState => {
+            return {
+                timesClicked: previousState.timesClicked+1
+            }
+        })
+    }
+
+    render() {
+        return(
+                <button onClick={this.clickHandler}>{this.state.timesClicked}</button>
+        )
+    }
+}
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,67 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+                video: {
+                    resolution: '1080p'
+              }
+            }
+        }
+    }
+
+    /* assigning state on multiple levels 
+        this.setState({
+            addressInfo: {
+                ...this.state.addressInfo,
+                city: 'New York City'
+            }
+        })
+
+        in this case addressInfo: is the first level
+        this.state.addressInfo picks up the data contained within addressInfo
+        city: 'New York City' either assigns or reassigns a value to the key of city
+    */
+
+    bitrateClickHandler = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    resClickHandler = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                    resolution: '720p'
+                }
+                // yes, if it's nested this far it needs to be reset from the second tier all the way in
+                // i.e. the first tier is this.state
+                // the second tier is this.state.settings
+                // and so on in.
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="bitrate" onClick={this.bitrateClickHandler}></button>
+                <button className="resolution" onClick={this.resClickHandler}></button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger
